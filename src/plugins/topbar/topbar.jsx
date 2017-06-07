d1c79f347ca3480f18bf3c1fec61e1268ed8edc2
Brad Seefeld <bradley.seefeld@gmail.com>
2017-06-07 11:47:42 -0700

    Apply lopez theming

--- src/plugins/topbar/topbar.jsx
+++ src/plugins/topbar/topbar.jsx
@@ -41,8 +41,7 @@ export default class Topbar extends React.Component {
           <div className="wrapper">
             <div className="topbar-wrapper">
               <Link href="#" title="Swagger UX">
-                <img height="30" width="30" src={ Logo } alt="Swagger UX"/>
-                <span>swagger</span>
+                <img height="30" width="150" src={ Logo } alt="Swagger UX"/>
               </Link>
               <form className="download-url-wrapper" onSubmit={this.downloadUrl}>
                 <input className="download-url-input" type="text" onChange={ this.onUrlChange } value={this.state.url} disabled={isLoading} style={inputStyle} />
